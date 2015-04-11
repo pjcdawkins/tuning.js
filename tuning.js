f01a3de4fd18d217759c648e6c894e806f1c6cb6
Patrick Dawkins <pjcdawkins@gmail.com>
2015-04-11 21:59:29 +0100

    Show quarter tones

--- tuning.js
+++ tuning.js
@@ -3,38 +3,38 @@
 'use strict';
 
 var program = require('commander');
+var noteNames = require('./note-names.js');
 
 /**
  * A note.
  *
- * @param string noteString
+ * @param noteString string
  *   A note represented by scientific pitch notation, e.g. C4.
  */
 var Note = function (noteString) {
-  console.assert(noteString.match(/^[A-Ga-g] ?([b#sf]|(sharp|flat))? ?[0-9]?$/), 'Invalid note "' + noteString + '"');
+  console.assert(noteString.match(/^[A-Ga-g] ?[\+\-]? ?([b#sf]|(sharp|flat))? ?[\+\-]? ?[0-9]?$/), 'Invalid note "' + noteString + '"');
   this.name = noteString[0].toUpperCase();
   this.accidental = noteString.replace(/^[A-Ga-g] ?[0-9]?/, '').replace('f', 'b').replace('s', '#')[0] || '';
   // @todo allow octaves below 0 or above 9
   this.octave = noteString.match(/[0-9]$/) ? parseInt(noteString.charAt(noteString.length - 1), 10) : 4;
-  this.pitchClass = this.pitchClasses[this.name + this.accidental];
+  this.cents = noteNames[this.name + this.accidental];
 };
 
 Note.prototype = {
-  pitchClasses: {'C': 0, 'C#': 1, 'Db': 1, 'D': 2, 'D#': 3, 'Eb': 3, 'E': 4, 'E#': 5, 'Fb': 4, 'F': 5, 'F#': 6, 'Gb': 6, 'G': 7, 'G#': 8, 'Ab': 8, 'A': 9, 'A#': 10, 'Bb': 10, 'B': 11, 'B#': 0, 'Cb': 11},
   toString: function () {
     return this.name + this.accidental + this.octave;
   },
   transpose: function (interval) {
-    var newPc = this.pitchClass + interval,
-      octaveMultiplier = Math.floor(newPc / 12),
+    var newCents = this.cents + interval * 100,
+      octaveMultiplier = Math.floor(newCents / 1200),
       newName,
       noteName;
-    if (newPc > 11 || newPc < 0) {
-      newPc -= 12 * octaveMultiplier;
+    if (newCents > 1150 || newCents < 0) {
+      newCents -= 1200 * octaveMultiplier;
     }
     // @todo sort out preference order
-    for (noteName in this.pitchClasses) {
-      if (this.pitchClasses[noteName] === newPc) {
+    for (noteName in noteNames) {
+      if (noteNames[noteName] === newCents) {
         newName = noteName;
         break;
       }
@@ -62,9 +62,9 @@ Temperament.prototype = {
   // @todo make this work for non-ET
   getFrequency: function (note) {
     var getSteps = function (fromNote, toNote) {
-        return toNote.pitchClass - fromNote.pitchClass + (12 * (toNote.octave - fromNote.octave));
+        return toNote.cents - fromNote.cents + (1200 * (toNote.octave - fromNote.octave));
       },
-      factor = Math.pow(2, 1 / 12),
+      factor = Math.pow(2, 1 / 1200),
       steps = getSteps(new Note('A4'), note);
     return this.A * Math.pow(factor, steps);
   }
@@ -72,17 +72,17 @@ Temperament.prototype = {
 
 if (require.main === module) {
   program
-    .option('-n, --note <string>', 'The name of the note.', String, 'C4')
-    .option('-f, --fundamental <string>', 'The name of the fundamental.', String, 'C2')
-    .option('-d, --division <int>', 'The octave division (12 for ET).', parseInt, 12)
+    .option('-n, --note <string>', 'The name of the note.', String, 'A4')
+    .option('-f, --fundamental <string>', 'The name of the fundamental.', String, 'A4')
+    .option('-d, --division <int>', 'The octave division (1200 for ET).', parseInt, 1200)
     .option('-A <int>', 'The frequency of A4 in Hz.', parseInt, 440)
     .parse(process.argv);
   var note = new Note(program.args[0] !== undefined ? program.args[0] : program.note),
     temperament = new Temperament(program.division, new Note(program.fundamental), program.A),
     newNote;
-  for (var i = 0; i <= 12; i++) {
-    newNote = note.transpose(i);
-    console.log("%s: %s Hz", newNote, temperament.getFrequency(newNote));
+  for (var i = 0; i <= 48; i++) {
+    newNote = note.transpose(i / 2);
+    console.log("%s\t%s Hz", newNote, Math.round(temperament.getFrequency(newNote) * 10) / 10);
   }
 }
 else {
