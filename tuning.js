baebb7ed7a8225ae7c69ed0ccd3cb2f07f81212d
Patrick Dawkins <pjcdawkins@gmail.com>
2016-01-23 12:40:21 +0000

    Various clean up + improvements

--- tuning.js
+++ tuning.js
@@ -1,91 +1,32 @@
-#!/usr/bin/env nodejs
+#!/usr/bin/env node
 
 'use strict';
 
 var program = require('commander');
-var noteNames = require('./note-names.js');
+var EqualTemperament = require('./lib/systems/et.js');
+var Note = require('./lib/note.js');
 
 /**
- * A note.
+ * Round a number to a given precision.
  *
- * @param noteString string
- *   A note represented by scientific pitch notation, e.g. C4.
- */
-var Note = function (noteString) {
-  console.assert(noteString.match(/^[A-Ga-g] ?[\+\-]? ?([b#sf]|(sharp|flat))? ?[\+\-]? ?[0-9]?$/), 'Invalid note "' + noteString + '"');
-  this.name = noteString[0].toUpperCase();
-  this.accidental = noteString.replace(/^[A-Ga-g] ?[0-9]?/, '').replace('f', 'b').replace('s', '#')[0] || '';
-  // @todo allow octaves below 0 or above 9
-  this.octave = noteString.match(/[0-9]$/) ? parseInt(noteString.charAt(noteString.length - 1), 10) : 4;
-  this.cents = noteNames[this.name + this.accidental];
-};
-
-Note.prototype = {
-  toString: function () {
-    return this.name + this.accidental + this.octave;
-  },
-  transpose: function (interval) {
-    var newCents = this.cents + interval * 100,
-      octaveMultiplier = Math.floor(newCents / 1200),
-      newName,
-      noteName;
-    if (newCents > 1150 || newCents < 0) {
-      newCents -= 1200 * octaveMultiplier;
-    }
-    // @todo sort out preference order
-    for (noteName in noteNames) {
-      if (noteNames[noteName] === newCents) {
-        newName = noteName;
-        break;
-      }
-    }
-    newName += (this.octave + octaveMultiplier);
-    return new Note(newName);
-  }
-};
-
-/**
- * A temperament system.
+ * @param {number} number
+ * @param {int=1} decimals
  *
- * @param int division
- * @param Note fundamental
- * @param int A
+ * @returns {number}
  */
-var Temperament = function (division, fundamental, A) {
-  this.division = division;
-  this.fundamental = fundamental;
-  this.smallestInterval = 1200 / this.division;
-  this.A = A;
-};
-
-Temperament.prototype = {
-  // @todo make this work for non-ET
-  getFrequency: function (note) {
-    var getSteps = function (fromNote, toNote) {
-        return toNote.cents - fromNote.cents + (1200 * (toNote.octave - fromNote.octave));
-      },
-      factor = Math.pow(2, 1 / 1200),
-      steps = getSteps(new Note('A4'), note);
-    return this.A * Math.pow(factor, steps);
-  }
-};
+function round(number, decimals) {
+    decimals = decimals || 1;
+    return decimals === 0
+        ? Math.round(number)
+        : Math.round(number * 10 * decimals) / (10 * decimals);
+}
 
 if (require.main === module) {
-  program
-    .option('-n, --note <string>', 'The name of the note.', String, 'A4')
-    .option('-f, --fundamental <string>', 'The name of the fundamental.', String, 'A4')
-    .option('-d, --division <int>', 'The octave division (1200 for ET).', parseInt, 1200)
-    .option('-A <int>', 'The frequency of A4 in Hz.', parseInt, 440)
-    .parse(process.argv);
-  var note = new Note(program.args[0] !== undefined ? program.args[0] : program.note),
-    temperament = new Temperament(program.division, new Note(program.fundamental), program.A),
-    newNote;
-  for (var i = 0; i <= 48; i++) {
-    newNote = note.transpose(i / 2);
-    console.log("%s\t%s Hz", newNote, Math.round(temperament.getFrequency(newNote) * 10) / 10);
-  }
-}
-else {
-  module.exports.Note = Note;
-  module.exports.Temperament = Temperament;
+    program
+        .option('-n, --note <string>', 'The name of the note.', String, 'A4')
+        .option('-A <int>', 'The frequency of A4 in Hz.', parseInt, 440)
+        .parse(process.argv);
+    var note = Note.fromString(program.args[0] !== undefined ? program.args[0] : program.note),
+        scale = new EqualTemperament(program.A);
+    console.log("%s\t%d Hz", note, round(scale.getFrequency(note)));
 }
